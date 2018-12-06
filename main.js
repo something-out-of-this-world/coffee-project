8d0b2caae3e11f646b69e413426f88e19dfee683
Justin <justin@Justins-MacBook-Pro.local>
2018-12-06 14:32:34 -0600

    replaced table with divs, made lists in ascending order, add bootstrap

--- main.js
+++ main.js
@@ -1,18 +1,16 @@
 "use strict"
 
 function renderCoffee(coffee) {
-    var html = '<tr class="coffee">';
-    html += '<td>' + coffee.id + '</td>';
-    html += '<td>' + coffee.name + '</td>';
-    html += '<td>' + coffee.roast + '</td>';
-    html += '</tr>';
-
+    var html = '<div class="coffee">';
+    html += '<div><h3>' + coffee.name + '</h3>';
+    html += '<p>' + coffee.roast + '</p></div>';
+    html += '</div>';
     return html;
 }
 
 function renderCoffees(coffees) {
     var html = '';
-    for(var i = coffees.length - 1; i >= 0; i--) {
+    for(var i = 0; i <= coffees.length -1; i++) {
         html += renderCoffee(coffees[i]);
     }
     return html;
@@ -24,10 +22,10 @@ function updateCoffees(e) {
     var filteredCoffees = [];
     coffees.forEach(function(coffee) {
         if (coffee.roast === selectedRoast) {
-            filteredCoffees.push(coffee);
+            filteredCoffees.unshift(coffee);
         }
     });
-    tbody.innerHTML = renderCoffees(filteredCoffees);
+    divbody.innerHTML = renderCoffees(filteredCoffees);
 }
 
 // from http://www.ncausa.org/About-Coffee/Coffee-Roasts-Guide
@@ -48,10 +46,10 @@ var coffees = [
     {id: 14, name: 'French', roast: 'dark'},
 ];
 
-var tbody = document.querySelector('#coffees');
+var divbody = document.querySelector('#coffees');
 var submitButton = document.querySelector('#submit');
 var roastSelection = document.querySelector('#roast-selection');
 
-tbody.innerHTML = renderCoffees(coffees);
+divbody.innerHTML = renderCoffees(coffees);
 
 submitButton.addEventListener('click', updateCoffees);
